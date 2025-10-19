f26ed53ad1307c0e4cfca0b46c9f43e60ca2eaa5
Franklin <ilamakovaleksej26@gmail.com>
2025-10-19 13:52:20 +0300

    Созданы классы Student и Teacher

--- Program.cs
+++ Program.cs
@@ -15,3 +15,47 @@
         Console.WriteLine($"ФИО: {fio}\nДата рождения: {dateOfBirth}\nПол: {sex} ");
     }
 }
+
+class Student : Person
+{
+    private int studentId;
+    private int yearOfStudy;
+    private string healthGroup;
+    public Student(int id, string fio, DateOnly dateOfBirth, string sex, int yearOfStudy, string healthGroup)
+        : base(fio, dateOfBirth, sex)
+    {
+        studentId = id;
+        this.yearOfStudy = yearOfStudy;
+        this.healthGroup = healthGroup;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Student");
+        base.PrintInfo();
+        Console.WriteLine($"ID студента: {studentId}\nГод обучения: {yearOfStudy}\nГруппа здоровья: {healthGroup}");
+    }
+}
+
+
+class Teacher : Person
+{
+    private int teacherID;
+    private int experience;
+    private string subjectArea;
+
+    public Teacher(int id, string fio, DateOnly dateOfBirth, string sex, int experience, string subjectArea)
+        : base(fio, dateOfBirth, sex)
+    {
+        teacherID = id;
+        this.experience = experience;
+        this.subjectArea = subjectArea;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Teacher");
+        base.PrintInfo();
+        Console.WriteLine($"ID: {teacherID}\nОпыт работы: {experience}\nПредметная область: {subjectArea}");
+    }
+}
