944f44c9279ae1920c782cd035471874c3374ab5
Franklin <ilamakovaleksej26@gmail.com>
2025-10-19 13:47:23 +0300

    Создан класс Person

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Person
+{
+    private string fio;
+    private DateOnly dateOfBirth;
+    private string sex;
+
+    public Person(string fio, DateOnly dateOfBirth, string sex)
+    {
+        this.fio = fio;
+        this.dateOfBirth = dateOfBirth;
+        this.sex = sex;
+    }
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"ФИО: {fio}\nДата рождения: {dateOfBirth}\nПол: {sex} ");
+    }
+}
