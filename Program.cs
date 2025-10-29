432ea1e5afb51d402160f24834eb8b7edc412a9d
Franklin <ilamakovaleksej26@gmail.com>
2025-10-29 16:22:37 +0300

    Добавлен класс Курс, меню и другие мелочи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,72 @@
-﻿class Person
+﻿
+using static System.Reflection.Metadata.BlobBuilder;
+
+List <Student> students = new List<Student>();
+List <Teacher> teachers = new List<Teacher>();
+List <Course> courses = new List<Course>();
+
+int studentGId = 0;
+int teacherGId = 0;
+int courseGId = 0;
+
+bool inMenu = true;
+
+while (inMenu)
+{
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Выберите пункт меню:");
+    Console.WriteLine("1. Добавить студента");
+    Console.WriteLine("2. Удалить студента по ID");
+    Console.WriteLine("3. Вывести информацию о всех судентах");
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("4. Добавить преподавателя");
+    Console.WriteLine("5. Удалить преподавателя по ID");
+    Console.WriteLine("6. Вывести информацию о всех преподавателях");
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("7. Добавить курс");
+    Console.WriteLine("8. Удалить курс по ID");
+    Console.WriteLine("9. Вывести информацию о всех курсах");
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("10. Записать студента на курс");
+    Console.WriteLine("11. Удалить студента с курса");
+    Console.WriteLine("12. Поменять преподователя на курсе");
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("0. Выход");
+    Console.WriteLine("-------------------------------------------");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("-------------------------------------------");
+}
+
+// Классы
+class Course
+{
+    public int id { get; private set; }
+    private string title;
+    private string description;
+    private int teacherId;
+    public List<int> studentId;
+
+    public Course(int id, string title, string description, int teacherId, List<int> studentId)
+    {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.teacherId = teacherId;
+        this.studentId = studentId;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Название: {title}\nОписание: {description}\nID преподаввателя: {teacherId}");
+        Console.WriteLine("Id студентов, записанных на курс:");
+        foreach (var sId in studentId)
+        {
+            Console.WriteLine(sId);
+        }
+    }
+}
+
+class Person
 {
     private string fio;
     private DateOnly dateOfBirth;
@@ -12,50 +80,54 @@
     }
     public virtual void PrintInfo()
     {
-        Console.WriteLine($"ФИО: {fio}\nДата рождения: {dateOfBirth}\nПол: {sex} ");
+        Console.WriteLine($"ФИО: {fio}\nДата рождения: {dateOfBirth}\nПол: {sex}");
     }
 }
 
 class Student : Person
 {
-    private int studentId;
+    public int id { get; private set; }
     private int yearOfStudy;
     private string healthGroup;
-    public Student(int id, string fio, DateOnly dateOfBirth, string sex, int yearOfStudy, string healthGroup)
+    public List<Course> courses;
+    public Student(int id, string fio, DateOnly dateOfBirth, string sex, int yearOfStudy, string healthGroup, List<Course> courses)
         : base(fio, dateOfBirth, sex)
     {
-        studentId = id;
+        this.id = id;
         this.yearOfStudy = yearOfStudy;
         this.healthGroup = healthGroup;
+        this.courses = courses;
     }
 
     public override void PrintInfo()
     {
         Console.WriteLine("Student");
         base.PrintInfo();
-        Console.WriteLine($"ID студента: {studentId}\nГод обучения: {yearOfStudy}\nГруппа здоровья: {healthGroup}");
+        Console.WriteLine($"ID студента: {id}\nГод обучения: {yearOfStudy}\nГруппа здоровья: {healthGroup}");
     }
 }
 
 
 class Teacher : Person
 {
-    private int teacherID;
+    public int id { get; private set; }
     private int experience;
     private string subjectArea;
-
-    public Teacher(int id, string fio, DateOnly dateOfBirth, string sex, int experience, string subjectArea)
+    public List<Course> courses;
+    public Teacher(int id, string fio, DateOnly dateOfBirth, string sex, int experience, string subjectArea, List<Course> courses)
         : base(fio, dateOfBirth, sex)
     {
-        teacherID = id;
+        this.id = id;
         this.experience = experience;
         this.subjectArea = subjectArea;
+        this.courses = courses;
     }
 
     public override void PrintInfo()
     {
         Console.WriteLine("Teacher");
         base.PrintInfo();
-        Console.WriteLine($"ID: {teacherID}\nОпыт работы: {experience}\nПредметная область: {subjectArea}");
+        Console.WriteLine($"ID: {id}\nОпыт работы: {experience}\nПредметная область: {subjectArea}");
     }
 }
+
